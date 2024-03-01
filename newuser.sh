b045292ddafb656382eb0fa0eab77e9adea55393
hebeleNhubele <abdullah.anbar@hotmail.com>
2024-03-01 20:13:15 +0200

    aaa

--- newuser.sh
+++ newuser.sh
@@ -22,17 +22,22 @@ usermod -aG sudo "$NEW_USER"
 # Configure passwordless sudo
 echo "$NEW_USER ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/$NEW_USER"
 
-# Enable SSH password authentication
+# Enable SSH password authentication, pubkey authentication, and ChallengeResponseAuthentication
 if [ -f /etc/ssh/sshd_config ]; then
     # CentOS
     sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+    sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+    sed -i 's/#ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/' /etc/ssh/sshd_config
 elif [ -f /etc/ssh/sshd_config ]; then
     # Ubuntu
     sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
+    sed -i 's/PubkeyAuthentication no/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+    sed -i 's/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/' /etc/ssh/sshd_config
 fi
 
 # Restart SSH service to apply changes
 service ssh restart
+systemctl restart sshd
 
 echo "User '$NEW_USER' has been created and added to sudoers with passwordless sudo access."
-echo "SSH password authentication has been enabled."
+echo "SSH password authentication, public key authentication, and ChallengeResponseAuthentication have been enabled."
